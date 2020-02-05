4d7b95173b55809cece25c69fb8b9df5754340c7
namankrs <snaman80@gmail.com>
2020-02-05 17:57:23 +0530

    Refactor

--- project.clj
+++ project.clj
@@ -5,8 +5,7 @@
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :profiles {:dev
-                     {:dependencies [[com.bhauman/rebel-readline "0.1.4"]
-                                     [lein-kibit "0.1.8"]]}
+                     {:dependencies [[com.bhauman/rebel-readline "0.1.4"]]}
              :kaocha {:dependencies [[lambdaisland/kaocha "0.0-573"]]}}
   :aliases {"kaocha" ["with-profile" "+kaocha" "run" "-m" "kaocha.runner"]}
   :repl-options {:init-ns assignments.core}
